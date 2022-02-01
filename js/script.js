06afb5d2e50d45c09515c9a21e842c8c41d1b123
ridoy2112 <you@example.com>
2022-02-02 03:44:13 +0600

    second commit

--- js/script.js
+++ js/script.js
@@ -2,13 +2,11 @@ let slider = document.querySelectorAll(".slide")
 let sliderArray = Array.from(slider)
 let prevbtn = document.querySelector(".prev")
 let nextbtn = document.querySelector(".next")
-let autoplay = document.querySelector(".autoplay")
-let sliderBody = document.querySelector(".slider")
-let note = document.querySelector(".note")
+let sliderBody = document.querySelector(".sliderBody")
 let prev
 let next
 let count = 0
-let countNumber = 0
+let stop
 function prevnext() {
     let active = document.querySelector(".active")
     let activeIndex = sliderArray.indexOf(active)
@@ -68,38 +66,38 @@ nextbtn.addEventListener("click", function () {
     next.classList.add("active")
     next.style.transform = "translateX(0)"
     transition()
-})
-sliderBody.addEventListener("click", function () {
+}) 
+function nextauto() {
+    let active = document.querySelector(".active")
+    let [prev, next] = prevnext()
+    active.classList.add("smooth")
+    next.classList.add("smooth")
+    active.classList.remove("active")
+    active.style.transform = "translateX(-100%)"
+    next.classList.add("active")
+    next.style.transform = "translateX(0)"
+    transition()
+}
+stop = setInterval(function () {
+    nextauto()
+}, 2000)
+sliderBody.addEventListener("mouseenter", function () {
     count++
     console.log(count)
-    function nextauto() {
-        if (count == 1) {
-            let active = document.querySelector(".active")
-            let [prev, next] = prevnext()
-            active.classList.add("smooth")
-            next.classList.add("smooth")
-            active.classList.remove("active")
-            active.style.transform = "translateX(-100%)"
-            next.classList.add("active")
-            next.style.transform = "translateX(0)"
-            transition()
-        }
-        else {
-            clearInterval(stop)
-            count = 0
-        }
-    }
-    let stop = setInterval(function () {
-        nextauto()
-    }, 2000)
-})
-function type() {
-    note.innerHTML += note.dataset.text.charAt(countNumber)
-    countNumber++
-    if (countNumber == note.dataset.text.length) {
+    if(count==1){
         clearInterval(stop)
+        count = 0
     }
-}
-let stop = setInterval(function () {
-    type()
-}, 20)
+    
+})
+
+sliderBody.addEventListener("mouseleave", function () {
+    count++
+    console.log(count)
+    if(count==1){  
+        stop = setInterval(function () {
+            nextauto()
+        }, 2000)
+        count = 0
+    }
+})
